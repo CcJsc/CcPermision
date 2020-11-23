b151e69245276880756f45d42f7412339c687a03
shengchao.jiang <shengchao.jiang@ecarx.com.cn>
2020-11-23 11:18:52 +0800

    fitst commit

---
+++ CcLibrary/src/main/java/com/example/cclibrary/CcTest.java
@@ -0,0 +1,17 @@
+package com.example.cclibrary;
+
+/**
+ * @author AlienChao
+ * @date 2020/11/23 11:10
+ */
+public class CcTest {
+
+    /**
+     * 计算乘以2的值
+     * @param num
+     * @return
+     */
+    public int calculation(int num) {
+        return num * 2;
+    }
+}
